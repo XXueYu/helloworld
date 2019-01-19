3ae1e6ad130e10626f6c42d70b60620fb6a231dc
xiao <15927006796@163.com>
2019-01-19 16:45:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+      @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button =(Button)findViewById(R.id.but1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String TAG ="xiaobudian";
+                Log.i(TAG, "onClick: ");
+            }
+        });
+    }
+}
